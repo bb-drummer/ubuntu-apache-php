2b1ac8a753f41126795f11a9dd88ddd93495549e
Björn Bartels <github@dragon-projects.net>
2019-03-19 18:59:58 +0100

    update test

--- test/test.sh
+++ test/test.sh
@@ -12,7 +12,6 @@ echo "RUN service apache2 restart" >> Dockerfile.${PHP_VERSION}
 echo "RUN ls -la /test/" >> Dockerfile.${PHP_VERSION}
 echo "RUN ls -la /var/www/html/" >> Dockerfile.${PHP_VERSION}
 echo "RUN cat /var/log/apache2/error.log" >> Dockerfile.${PHP_VERSION}
-echo "RUN cat /var/log/apache2/error.log" >> Dockerfile.${PHP_VERSION}
 
 docker build -t ci/base:${PHP_VERSION} -f ../${PHP_VERSION}/Dockerfile ../
 docker build -t ci/test:${PHP_VERSION} -f Dockerfile.${PHP_VERSION} .
@@ -21,6 +20,7 @@ CID=`docker run -d -p 80:80 ci/test:${PHP_VERSION}`
 
 # wait for start of apache
 sleep 15
-curl -vf http://127.0.0.1:80
+curl -vf localhost
+#curl -vf http://172.18.0.2:80
 
 docker stop $CID
