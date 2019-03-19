215df2cd25263e903e50cdbe55c458952a9f19ea
Björn Bartels <github@dragon-projects.net>
2019-03-19 20:56:56 +0100

    clean up

--- test/test.sh
+++ test/test.sh
@@ -8,23 +8,17 @@ echo "FROM ci/base:${PHP_VERSION}" >> Dockerfile.${PHP_VERSION}
 echo "RUN rm -f /var/www/html/index.html" >> Dockerfile.${PHP_VERSION}
 ##echo "ADD code /var/www/html" >> Dockerfile.${PHP_VERSION}
 echo "RUN cp /test/index.php /var/www/html/" >> Dockerfile.${PHP_VERSION}
-echo "RUN service apache2 restart" >> Dockerfile.${PHP_VERSION}
-echo "RUN cat /var/log/apache2/error.log" >> Dockerfile.${PHP_VERSION}
-echo "RUN apache2ctl -S" >> Dockerfile.${PHP_VERSION}
-echo "RUN ifconfig" >> Dockerfile.${PHP_VERSION}
 
 docker build -t ci/base:${PHP_VERSION} -f ../${PHP_VERSION}/Dockerfile ../
 docker build -t ci/test:${PHP_VERSION} -f Dockerfile.${PHP_VERSION} .
 
+# run the container
 CID=`docker run -d -p 80:80 ci/test:${PHP_VERSION}`
-#docker run -d --name php-ci-test -p 80:80 ci/test:${PHP_VERSION}
+echo "Docker Container ID: $CID"
 
-#echo "Docker Container ID: $CID"
 # wait for start of apache
-sleep 30
+sleep 15
 #curl -vf localhost
 curl -vf php-ci-test:80
-#curl -vf http://172.18.0.2
 
 docker stop $CID
-#docker stop php-ci-test
