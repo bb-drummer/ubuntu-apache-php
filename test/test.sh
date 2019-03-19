98a5a31a75d808ad506550e3ea61bfe9b6becb3d
Björn Bartels <github@dragon-projects.net>
2019-03-19 16:18:38 +0100

    update docker files, test script

--- test/test.sh
+++ test/test.sh
@@ -7,7 +7,7 @@ cd `dirname $0`
 echo "FROM ci/base:${PHP_VERSION}" >> Dockerfile.${PHP_VERSION}
 echo "RUN rm -f /var/www/html/index.html" >> Dockerfile.${PHP_VERSION}
 ##echo "ADD code /var/www/html" >> Dockerfile.${PHP_VERSION}
-echo "ADD test/index.php /var/www/html/" >> Dockerfile.${PHP_VERSION}
+echo "RUN cp /test/index.php /var/www/html/" >> Dockerfile.${PHP_VERSION}
 
 docker build -t ci/base:${PHP_VERSION} -f ../${PHP_VERSION}/Dockerfile ../
 docker build -t ci/test:${PHP_VERSION} -f Dockerfile.${PHP_VERSION} .
