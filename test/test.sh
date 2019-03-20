e7d2c70e6b0ff8dd5d403addddda63ad37e01a28
Björn Bartels <github@dragon-projects.net>
2019-03-20 13:20:52 +0100

    update test script

--- test/test.sh
+++ test/test.sh
@@ -5,15 +5,16 @@ PHP_VERSION=$1
 cd `dirname $0`
 
 echo "FROM ci/base:${PHP_VERSION}" >> Dockerfile.${PHP_VERSION}
-echo "RUN rm -f /var/www/html/index.html" >> Dockerfile.${PHP_VERSION}
+
+#echo "RUN rm -f /var/www/html/index.html" >> Dockerfile.${PHP_VERSION}
 ##echo "ADD code /var/www/html" >> Dockerfile.${PHP_VERSION}
-echo "RUN cp /test/index.php /var/www/html/" >> Dockerfile.${PHP_VERSION}
+#echo "RUN cp /test/index.php /var/www/html/" >> Dockerfile.${PHP_VERSION}
 
 docker build -t ci/base:${PHP_VERSION} -f ../${PHP_VERSION}/Dockerfile ../
 docker build -t ci/test:${PHP_VERSION} -f Dockerfile.${PHP_VERSION} .
 
 # run the container
-CID=`docker run -d -p 80:80 ci/test:${PHP_VERSION}`
+CID=`docker run --rm -d -p 80:80 ci/test:${PHP_VERSION}`
 echo "Docker Container ID: $CID"
 
 # wait for start of apache
