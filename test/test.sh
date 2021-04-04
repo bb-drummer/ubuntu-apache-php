f8b511f6b2faad9c1d3e286e29bb229c19f6e839
Björn Bartels <bartels@fr-k.de>
2021-04-04 15:11:44 +0200

    upd circle CI cfg

--- test/test.sh
+++ test/test.sh
@@ -32,7 +32,7 @@ sleep 15
 
 if [[ "$TEST_IP" == "none" ]]; then
     HOST_IP=$(ifconfig | grep -Eo 'inet (addr:)?([0-9]*\.){3}[0-9]*' | grep -Eo '([0-9]*\.){3}[0-9]*' | grep -v '127.0.0.1')
-    curl -vf http://$HOST_IP:$TEST_PORT/index.php
+    curl -vf http://127.0.0.1:$TEST_PORT/index.php
 else
     curl -vf http://$TEST_IP:$TEST_PORT/index.php
 fi;
