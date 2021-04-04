802294e58bcab9e2d1054383e4c6f95d855304c5
Björn Bartels <coding@bjoernbartels.earth>
2021-04-04 11:25:18 +0200

    Update test/test.sh, .gitlab-ci.yml, 8.0/hooks/pre_build, 8.0/Dockerfile files

--- test/test.sh
+++ test/test.sh
@@ -7,6 +7,9 @@ cd `dirname $0`
 if [[ -z "$TEST_IP" ]]; then
     TEST_IP=0.0.0.0;
 fi;
+if [[ -z "$TEST_PORT" ]]; then
+    TEST_PORT=9876;
+fi;
 
 echo "FROM ci/base:${PHP_VERSION}" >> Dockerfile.${PHP_VERSION}
 
@@ -18,7 +21,7 @@ docker build -t ci/base:${PHP_VERSION} -f ../${PHP_VERSION}/Dockerfile ../
 docker build -t ci/test:${PHP_VERSION} -f Dockerfile.${PHP_VERSION} .
 
 # run the container
-CID=`docker run --rm -d -p 9876:80 ci/test:${PHP_VERSION}`
+CID=`docker run --rm -d -p $TEST_PORT:80 ci/test:${PHP_VERSION}`
 echo "Docker Container ID: $CID"
 CONTAINER_IP=`docker inspect -f '{{range .NetworkSettings.Networks}}{{.IPAddress}}{{end}}' ${CID}`
 echo "Docker Container IP: $CONTAINER_IP"
@@ -26,6 +29,6 @@ echo "Docker Container IP: $CONTAINER_IP"
 # wait for start of apache
 sleep 15
 
-curl -vf http://$TEST_IP:9876/index.php
+curl -vf http://$TEST_IP:$TEST_PORT/index.php
 
 docker stop $CID
